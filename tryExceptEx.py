407e73a2deb16c588d3e378f5e6e6f481ec679a5
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-01-04 13:07:01 -0800

    tryExceptEx

---
+++ tryExceptEx.py
@@ -0,0 +1,10 @@
+try:
+    num = int(input("put your random number"))
+except:
+    print("you forgot to put your number")
+if num > 100:
+    print("the number is greater than 100")
+if num % 2 == 0:
+    print("the number is even")
+else:
+    print("thanks for putting your number. bye!")
